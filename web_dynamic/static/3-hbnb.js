d3183f5c09dd2c8069f9454f584204c6c94a49c7
EMarvelM <marvelousegbe19@gmail.com>
2024-05-06 14:19:57 +0100

    implementing a get request to add a color to the icon when api is succesful

---
+++ web_dynamic/static/3-hbnb.js
@@ -0,0 +1,50 @@
+$.get({
+    "url":"http://0.0.0.0:5001/api/v1/status/",
+    "success": (response) => {
+        if (response.status === "OK"){
+            $('DIV#api_status').addClass("available");
+        } else {
+            $('DIV#api_status').removeClass("available")
+        }
+    }
+})
+
+let amenity_id = [];
+$('document').ready(
+    () => {
+        $("div.popover ul li input").on(
+            "click", function () {
+
+                let $checkbox = $(this)
+
+                if ($(this).is(":checked")){
+                    amenity_id.push($(this).attr('data-name'));
+                } else {
+                    $.each(amenity_id, function (index, item){
+                            console.log(item)
+                            
+                        if (item == $checkbox.attr('data-name') && index != -1){
+                            amenity_id.splice(index, 1)
+                        }
+                    })
+                }
+                $('div.amenities h4').empty()
+                for (a_id of amenity_id){
+                    if (amenity_id) {
+                        if (a_id != amenity_id[0]) {
+                            $('div.amenities h4').append(", ")
+                        }
+                    }
+                    $('div.amenities h4').append(a_id.replace(/:/g, ""))
+
+                    if ($('div.amenities h4').text().length > 31) {
+                        //console.log('greater than 34')
+                        $('div.amenities h4').text($('div.amenities h4').text().substring(0, 31))
+                        $('div.amenities h4').append("...")
+                    }
+                }
+            }
+        )
+    }
+);
+
